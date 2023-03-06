77a42d94229e570eedf94d9f404d14df6caf3ca8
Matheus Mendes <matheusmendes@Laptop-de-Matheus.local>
2023-03-06 14:45:11 -0300

    create ui

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -49,7 +49,7 @@ export function SummaryTable() {
             </div>
 
             <div className="grid grid-rows-7 grid-flow-col gap-3">
-                {sumaryDates.map(date => {
+                {summary.length > 0 && sumaryDates.map(date => {
                     const dayInSummary = summary.find(day => {
                         return dayjs(date).isSame(day.date, 'day')
                     })
@@ -58,7 +58,7 @@ export function SummaryTable() {
                     key={date.toString()}
                     date={date}
                     amount={dayInSummary?.amount} 
-                    completed={dayInSummary?.completed}
+                    defaultCompleted={dayInSummary?.completed}
                      />
                 })}
 
